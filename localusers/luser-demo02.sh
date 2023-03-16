9634173ee657abf65821c67c24685fc17aa7be80
Murat Muhammet Savcı <murat.m.savci@gmail.com>
2023-03-16 03:21:18 +0300

    kod güncelleme

--- localusers/luser-demo02.sh
+++ localusers/luser-demo02.sh
@@ -5,8 +5,22 @@
 # Display if the user is the root user or not.
 
 # Display the UID
-echo "Your UID is ${UID}"
+echo "1. Your UID is ${UID}"
 
 # Display 
+USER_NAME=$(id -un)
+echo "2. your username is $USER_NAME"
+echo "again"
+echo "3. Your username is ${USER_NAME}"
 
+USER_NAME2=`id -un` # older style 
+echo "different wring style in code"
+echo "4. Your username is ${USER_NAME2}"
 
+# Display if the user is the root user or not
+if [[ "${UID}" -eq 0 ]]
+then
+    echo 'You are root.'
+else
+    echo 'You are not root'
+fi
