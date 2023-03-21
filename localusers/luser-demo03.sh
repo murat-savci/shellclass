9b0df9e89ee782985f408df1e815b424c9be6eca
Murat Muhammet Savcı <murat.m.savci@gmail.com>
2023-03-21 16:29:44 +0300

    script completed.

--- localusers/luser-demo03.sh
+++ localusers/luser-demo03.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
-# Empty file
+
+# Display the UID and username of the user executing this script.
+# Display if the user is savcimu user or not.
+
+# Important Note:
+# In Bash, -eq is used for numeric comparison, while == is used for string comparison. 
+# Using -eq for string comparison will result in an error.
+
+user_id=$(id -u)
+username=$(id -un)
+
+# Display username
+echo "Hello your username is $username"
+sleep 1
+echo "Your user ID is $user_id" 
+
+if [[ $username == "savcimu" ]]
+then
+    echo "it is savcimu account!"
+elif [[ $username == "root" ]]
+then
+    echo "it is root account"
+else
+    echo "it is another acount"
+fi
+
+# Test
+# echo "${user_id}"
+# echo "$username"sudo pass
