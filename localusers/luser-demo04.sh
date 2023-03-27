70a453c953765801b295265930be4d59d61d20d5
Murat Muhammet Savcı <murat.m.savci@gmail.com>
2023-03-27 03:55:25 +0300

    some info added

--- localusers/luser-demo04.sh
+++ localusers/luser-demo04.sh
@@ -3,3 +3,27 @@
 # This script creates an account on the local system.
 # You will be prompted for the accoun name and password.
 
+# Ask for username
+
+
+
+# Ask for the real name
+
+
+
+# Ask for the password
+
+
+
+# Create the user
+
+
+
+# Set password for the user
+
+
+
+
+# Force password change on first login.
+
+
